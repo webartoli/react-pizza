2e336c6bd64712a635c97d4d26f62b7b636e5870
webartoli <claudio@barto.li>
2020-01-02 19:47:55 +0100

    wip ingredient selections

--- components/ingredients.tsx
+++ components/ingredients.tsx
@@ -7,26 +7,55 @@ interface IngredientsProps {
   setIngredients: any
 }
 
+interface IngredientRowProps {
+  name: string
+  selected: boolean
+  setIngredient
+  unsetIngredient
+}
+
+const IngredientRow = (props: IngredientRowProps) => {
+
+  const toggleIngredient = () => {
+    if (props.selected) 
+      props.unsetIngredient() 
+    else
+      props.setIngredient() 
+  }
+
+  return (
+    <li onClick={_ => toggleIngredient()}>
+    {props.selected ? 'X': '-'} {props.name}
+    </li>
+  )
+}
+
 export const ChooseIngredients = (props: IngredientsProps) => {
 
   const ingredients = props
     .availableIngredients
     .map(x => ({
       name: x,
-      selected: props.ingredients.some(y => y === x)
+      selected: props.ingredients.some(y => y === x),
+      setIngredient: () => {
+        let newVal = [...ingredients, x]
+        console.log(newVal)
+        props.setIngredients(newVal)
+      },
+      unsetIngredient: () => {
+        let list = [...props.ingredients]
+        list.splice( list.indexOf(x), 1 )
+        props.setIngredients(x)
+        }
     }))
-
-  const toggleIngredient = (ingredient: string) => {
-    const selected = props.ingredients.some(y => y === ingredient)
-
-  }
+    .map(x => <IngredientRow {...x} />)
 
  return (
   <div>
     <h2>Ciao {props.name}</h2>
     <span>Scegli gli ingredienti...</span>
     <ul>
-      { ingredients.map(x => <li onClick={_ => toggleIngredient(x.name)}>{x.selected ? 'X': '-'} {x.name}</li>) }
+      { ingredients }
     </ul>
     <button>Avanti ></button>
   </div>
