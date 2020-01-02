b90838dccae4f413641b4a873e32c883d6d518ae
webartoli <claudio@barto.li>
2020-01-02 18:31:44 +0100

    introdocing react router

--- index.tsx
+++ index.tsx
@@ -2,13 +2,78 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 interface AppProps { }
 interface AppState {
   name: string;
 }
 
-class App extends Component<AppProps, AppState> {
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/old">
+            <App2 />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Nav() {
+  return  <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/old">Old App</Link>
+            </li>
+          </ul>
+        </nav>;
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+class App2 extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
