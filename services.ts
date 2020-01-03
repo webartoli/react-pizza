b17b1317f67d6677588440e4bdaffc2ca570de19
webartoli <claudio@barto.li>
2020-01-03 18:05:34 +0100

    firebase integration

--- services.ts
+++ services.ts
@@ -1,4 +1,17 @@
 
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+if (!firebase.apps.length) 
+  firebase.initializeApp({
+    apiKey: "AIzaSyAR3-QXrpEa9O62TtUcgqWwEiudtLYjmEw",
+    authDomain: "pizza-c568d.appspot.com",
+    projectId: "pizza-c568d"
+  });
+
+var db = firebase.firestore();
+export const ingredients = async () => db.collection('ingredients').get()
+
 export const availableIngredients = () => [
     // Base
     'Pomodoro', 'Mozzarella', 
