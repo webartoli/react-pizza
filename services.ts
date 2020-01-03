5b60588b8cb1ccda6bf3e7ae25b08787b7bc1fd6
webartoli <claudio@barto.li>
2020-01-03 18:10:32 +0100

    config changes

--- services.ts
+++ services.ts
@@ -5,8 +5,13 @@ import 'firebase/firestore'
 if (!firebase.apps.length) 
   firebase.initializeApp({
     apiKey: "AIzaSyAR3-QXrpEa9O62TtUcgqWwEiudtLYjmEw",
-    authDomain: "pizza-c568d.appspot.com",
-    projectId: "pizza-c568d"
+    authDomain: "pizza-c568d.firebaseapp.com",
+    databaseURL: "https://pizza-c568d.firebaseio.com",
+    projectId: "pizza-c568d",
+    storageBucket: "pizza-c568d.appspot.com",
+    messagingSenderId: "189632465397",
+    appId: "1:189632465397:web:d03f87c4ca4106734678bc",
+    measurementId: "G-XGP6HHNRCD"
   });
 
 var db = firebase.firestore();
@@ -24,3 +29,11 @@ export const availableIngredients = () => [
     // Sottoli e sottaceti
     'Capperi', 'Olive', 'Acciughe'
   ]
+
+const fire = async () => {
+  ingredients()
+    .then(data => console.log(data))
+    .catch(err => console.warn(err))
+}
+
+fire()
