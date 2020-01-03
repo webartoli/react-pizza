38aa5f489e368179eb20c03d4356a02fcc9299b7
webartoli <claudio@barto.li>
2020-01-03 16:06:52 +0100

    clean code

--- index.tsx
+++ index.tsx
@@ -5,11 +5,14 @@ import './style.css';
 import { WhoAreYou } from './components/who';
 import { Summary, Confirm } from './components/summary';
 import { ChooseIngredients } from './components/ingredients'
+import * as services from './services'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  Redirect,
+  NoMatch
 } from "react-router-dom";
 
 interface AppProps { }
@@ -17,109 +20,42 @@ interface AppState {
   name: string;
 }
 
-
-
-
 export default function App() {
 
   let [name, setName] = useState('')
   let selectedIngredients = useState<string[]>([])
 
-  let availableIngredients = [
-    'Pomorodo', 'Mozzarella', 'Capperi', 'Acciughe'
-  ]
+  const availableIngredients = services.availableIngredients()
+
 
   return (
     <Router>
+      <h1>Pizza BHO</h1>
       <div>
-        <Nav />
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/old">
-            <App2 />
-          </Route>
-          <Route path="/who">
+          <Route path="/who"  >
             <WhoAreYou name={name} setName={setName} />
           </Route>
           <Route path="/confirmed">
             <Confirm name={name} />
           </Route>
            <Route path="/compose">
-            <ChooseIngredients name={name} availableIngredients= {availableIngredients} selectedIngredients={selectedIngredients} />
+            <ChooseIngredients name={name} availableIngredients={availableIngredients} selectedIngredients={selectedIngredients} />
           </Route>
           <Route path="/summary">
             <Summary name={name} ingredients={selectedIngredients[0]} />
           </Route>
-          <Route path="/">
-            <Home />
+          <Redirect from="/" to="/who" />
+          <Route>
+            <NoMatch />
           </Route>
-         
         </Switch>
       </div>
     </Router>
   );
-}
-
-function Nav() {
-  return  <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-            <li>
-              <Link to="/old">Old App</Link>
-            </li>
-            <li>
-              <Link to="/who">Step 1</Link>
-            </li>
-          </ul>
-        </nav>;
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
-
-class App2 extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
 }
 
 render(<App />, document.getElementById('root'));
---
+++ services.ts
@@ -0,0 +1,13 @@
+
+export const availableIngredients = () => [
+    // Base
+    'Pomodoro', 'Mozzarella', 
+    // Affettati
+    'Prosciutto', 'Crudo', 'Salsiccia', 'Wurstel',  'Speck', 'Salamino Piccante', 'Pancetta',
+    // Verdure
+    'Funghi', 'Cipolla', 'Verdure grigliate',
+    // Formaggi
+    'Scamorza',
+    // Sottoli e sottaceti
+    'Capperi', 'Olive', 'Acciughe'
+  ]
